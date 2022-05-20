69af5104810dbafd83b5ef262f2d64d4c56f5970
Yuji Fujiwara <waratyo@gmail.com>
2022-05-20 13:24:38 +0900

    微調整

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -67,7 +67,6 @@ export default function DefaultLayout({ children }: LayoutParam) {
                 onClick={toggleDrawer}
                 sx={{
                   marginRight: '36px',
-                  ...(openDrawer && { display: 'none' }),
                 }}
               >
                 <MenuIcon />
